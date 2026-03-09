731f80ee1df90fe00d378911951197233aac8406
Isuru Shehara <isurushehara@gmail.com>
2026-03-09 11:05:07 +0530

    refactor: Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and support for Windows 10 and 11.
    - Implemented utility functions in utils.cpp and utils.h for creating a console, redirecting output streams, and handling command line arguments.
    - Developed the Win32Window class in win32_window.cpp and win32_window.h to manage high DPI-aware windows, including theme updates based on system preferences.
    - Added functionality for automatic non-client DPI scaling and improved window management.

---
+++ frontend/lib/screens/login_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Expense Tracker Login")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          children: [
+
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {
+
+              },
+              child: const Text("Login"),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
